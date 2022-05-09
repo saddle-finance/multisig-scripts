5a78db45ed7430373b43298683ac920a0b70d366
David Hamme <dhamme@gmail.com>
2022-05-09 14:44:45 -0700

    correct sip10.2 txn

--- scripts/2022_05_05_1_sip_10_update_pool_params.py
+++ scripts/2022_05_05_1_sip_10_update_pool_params.py
@@ -26,12 +26,10 @@ def main():
     for pool_addr, A in pools_to_future_a.items():
         contract = multisig.contract(pool_addr)
         contract.rampA(A, now_plus_17_days_seconds)
-        if pool_addr == "0xaCb83E0633d6605c5001e2Ab59EF3C745547C8C7":
-            contract.setSwapFee(3e6)
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
-    safe_tx.safe_nonce = 26
+    safe_tx.safe_nonce = 30
 
     # sign with private key
     safe_tx.sign(deployer.private_key)
