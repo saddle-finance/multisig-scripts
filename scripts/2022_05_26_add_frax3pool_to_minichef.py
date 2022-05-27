8a2f731c00f3e397bd123fe2f6dd582509868243
Michael Daly <dalymike102@gmail.com>
2022-05-27 11:58:17 -0400

    Fixed script, added pythonic variables.

--- scripts/2022_05_26_add_frax3pool_to_minichef.py
+++ scripts/2022_05_26_add_frax3pool_to_minichef.py
@@ -1,3 +1,4 @@
+from multiprocessing import pool
 from helpers import CHAIN_IDS, MINICHEF_ADDRESSES, MULTISIG_ADDRESSES, SDL_ADDRESSES
 from ape_safe import ApeSafe
 from brownie import accounts, network
@@ -13,33 +14,27 @@ def main():
     multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
 
     minichef = multisig.contract(MINICHEF_ADDRESSES[CHAIN_IDS["MAINNET"]])
-    poolLength = minichef.poolLength()
-    newPoolIndex = poolLength + 1
-    minichef.massUpdatePools(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+    pool_length = minichef.poolLength()
+    pids = [x for x in range(1, pool_length)]
 
     # Static
-    currentSDLPerSecond = minichef.saddlePerSecond()
-    frax3poolSDLPerSecond = (2_000_000 * 1e18) / (2 * 4 * 7 * 24 * 60 * 60)
+    current_SDL_per_second = minichef.saddlePerSecond()
+    frax3pool_SDL_per_second = (2_000_000 * 1e18) / (2 * 4 * 7 * 24 * 60 * 60)
     totalAlloc = minichef.totalAllocPoint()
 
     # Calculation
-    newSDLPerSecond = currentSDLPerSecond + frax3poolSDLPerSecond
-    fraxAllocationRate = frax3poolSDLPerSecond / newSDLPerSecond
-    fraxAlloc = round((totalAlloc * fraxAllocationRate) / (1 - fraxAllocationRate))
-    print("fraxAlloc: ", fraxAlloc)
-    print("newPoolIndex: ", newPoolIndex)
+    new_SDL_per_second = current_SDL_per_second + frax3pool_SDL_per_second
+    frax_allocation_rate = frax3pool_SDL_per_second / new_SDL_per_second
+    frax_alloc = round((totalAlloc * frax_allocation_rate) / (1 - frax_allocation_rate))
 
     # State Change
-    minichef.setSaddlePerSecond(newSDLPerSecond)
+    minichef.massUpdatePools(pids)
+    minichef.setSaddlePerSecond(new_SDL_per_second)
     minichef.add(
-        newPoolIndex,
+        frax_alloc,
         "0x0785aDDf5F7334aDB7ec40cD785EBF39bfD91520",
         "0x0000000000000000000000000000000000000000",
     )
-    minichef.set(
-        newPoolIndex, fraxAlloc, "0x0000000000000000000000000000000000000000", False
-    )
-    minichef.updatePool(newPoolIndex)
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
