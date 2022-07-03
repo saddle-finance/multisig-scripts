2d02407ab58b3c88aa88f20a1817f1c05aa7947a
Michael Daly <dalymike102@gmail.com>
2022-07-03 19:05:54 -0400

    Run new alUSD gauge script with updated address

--- scripts/2022_07_01_add_new_alUSD_gauge_and_set_killed_on_old_gauge.py
+++ scripts/2022_07_01_add_new_alUSD_gauge_and_set_killed_on_old_gauge.py
@@ -4,32 +4,36 @@ from brownie import accounts, network
 
 from scripts.utils import confirm_posting_transaction
 
+
 def main():
     """Adds new alUSD gauge and sets 'is_killed' on old alUSD gauge"""
 
     print(f"You are using the '{network.show_active()}' network")
-    deployer = accounts.load("deployer") # prompts for password
+    deployer = accounts.load("deployer")  # prompts for password
     multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
-    
-    OLD_ALUSD_ETH_MAINNET_METAPOOL_GAUGE_ADDRESS = "0x3b35a3f9163cf5733837c5b51b1df493c340f8e3"
+
+    OLD_ALUSD_ETH_MAINNET_METAPOOL_GAUGE_ADDRESS = (
+        "0x3B35a3f9163CF5733837c5B51B1dF493C340F8E3"
+    )
 
     gauge_controller = multisig.contract(GAUGE_CONTROLLER_ADDRESS[CHAIN_IDS["MAINNET"]])
     old_alusd_gauge = multisig.contract(OLD_ALUSD_ETH_MAINNET_METAPOOL_GAUGE_ADDRESS)
 
-    # new alUSD gauge to add to gauge controller 
+    # new alUSD gauge to add to gauge controller
     # TODO: insert new gauge's address
     gauge_data = {
-        "SaddleFRAXalUSDMetaPoolGauge" : 
-            {"address" : "0x0",
-             "type" : 0,
-             "weight" : 0 },
+        "SaddleFRAXalUSDMetaPoolGauge": {
+            "address": "0x953693DCB2E9DDC0c1398C1b540b81b63ceA5e16",
+            "type": 0,
+            "weight": 0,
+        },
     }
 
     # add gauge to gauge controller
     gauge_controller.add_gauge(
         gauge_data["SaddleFRAXalUSDMetaPoolGauge"]["address"],
         gauge_data["SaddleFRAXalUSDMetaPoolGauge"]["type"],
-        gauge_data["SaddleFRAXalUSDMetaPoolGauge"]["weight"]
+        gauge_data["SaddleFRAXalUSDMetaPoolGauge"]["weight"],
     )
 
     # set 'is_killed' on old alUSD gauge
