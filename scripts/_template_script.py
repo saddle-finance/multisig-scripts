5aa1907ed0f1f65e011409c02982458ce7ec8f06
hannespfeiffer <hannes.pfeiffer@gmx.at>
2022-07-06 01:12:04 +0200

    Add nonce checking logic - nonce too low - nonce already in use - nonce too high

--- scripts/_template_script.py
+++ scripts/_template_script.py
@@ -10,20 +10,39 @@ def main():
     print(f"You are using the '{network.show_active()}' network")
     deployer = accounts.load("deployer") # prompts for password
     multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
-
+   
     """
-
+   
     Build your txn here
-
+   
     """
 
     # combine history into multisend txn
-    # TODO: change nonce to current multisig nonce + 1
+    # TODO: set nonce 
     safe_tx = multisig.multisend_from_receipts()
-    safe_tx.safe_nonce = 0
+    safe_nonce = 0
+    
+    # check if nonce is invalid or already in use
+    current_nonce = multisig.contract(multisig.address).nonce()
+    pending_nonce = multisig.pending_nonce()
+    
+    if safe_nonce < current_nonce:
+        print(f"Error: Your nonce ({safe_nonce}) is already used. " + 
+            f"Please use a nonce greater equal {current_nonce}.")
+        return  
+    if safe_nonce < pending_nonce:
+        if (input(f"There is already a pending transaction at nonce {safe_nonce}. " + 
+            "Are you sure you want to use duplicate nonce for this submission? [y/N]")) == "N":
+            return
+    elif safe_nonce > pending_nonce:
+        if (input(f"Your nonce ({safe_nonce}) is greater than current pending nonce " + 
+            f"({pending_nonce}). Tx won't execute until intermediate nonces have " + 
+            "been used. Are you sure you want to post submission? [y/N]")) == "N":
+            return
+
+    safe_tx.safe_nonce = safe_nonce
 
     # sign with private key
     safe_tx.sign(deployer.private_key)
     multisig.preview(safe_tx)
-
     confirm_posting_transaction(multisig, safe_tx)
