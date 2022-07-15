82918902c1f0f20f2524b1d44ab1e80491796de3
Michael Daly <dalymike102@gmail.com>
2022-07-15 18:07:48 -0400

    added nonce

--- scripts/2022_07_06_update_vesting_beneficiary.py
+++ scripts/2022_07_06_update_vesting_beneficiary.py
@@ -3,31 +3,34 @@ from ape_safe import ApeSafe
 from brownie import accounts, network, Contract
 import datetime
 
-from scripts.utils import confirm_posting_transaction
+from scripts.utils import confirm_posting_transaction, fetch_current_nonce
+
 
 def main():
     """Release tokens and shift beneficiary for vesting contract of outgoing employee"""
 
     print(f"You are using the '{network.show_active()}' network")
 
-    if (datetime.datetime.now() < datetime.datetime(2022,7,15)):
+    if datetime.datetime.now() < datetime.datetime(2022, 7, 15):
         print("Too early to execute, returning")
         return
 
-    deployer = accounts.load("deployer") # prompts for password
+    deployer = accounts.load("deployer")  # prompts for password
     multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
 
     vesting_contract = Contract.from_abi(
         "Vesting",
         "0x85f99b73d0edd9cDb3462C94Ebe4c5758684BDf1",
         VESTING_ABI,
-        multisig.account
+        multisig.account,
     )
     vesting_contract.release()
     vesting_contract.changeBeneficiary(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
+    safe_tx.current_nonce = fetch_current_nonce(multisig)
+    safe_tx.safe_nonce = 49
 
     # sign with private key
     safe_tx.sign(deployer.private_key)
