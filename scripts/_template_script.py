69bec1a8a947991ea2ec96bb4fe33aa7e20f93c1
Hannes P <hannes.pfeiffer@gmx.at>
2022-07-28 21:10:48 +0200

    Add `multisig.preview_pending()` to template

--- scripts/_template_script.py
+++ scripts/_template_script.py
@@ -3,13 +3,17 @@ from ape_safe import ApeSafe
 from brownie import accounts, network
 from scripts.utils import confirm_posting_transaction
 
+
 def main():
     """DESCRIPTION OF WHAT THIS SCRIPT DOES"""
 
     print(f"You are using the '{network.show_active()}' network")
-    deployer = accounts.load("deployer") # prompts for password
+    deployer = accounts.load("deployer")  # prompts for password
     multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
-   
+
+    # Run any pending transactions before simulating any more transactions
+    multisig.preview_pending()
+
     """
    
     Build your txn here
@@ -17,10 +21,10 @@ def main():
     """
 
     # combine history into multisend txn
-    # TODO: set 'safe_nonce' 
+    # TODO: set 'safe_nonce'
     safe_tx = multisig.multisend_from_receipts()
     safe_nonce = 0
-    
+
     safe_tx.safe_nonce = safe_nonce
 
     # sign with private key
