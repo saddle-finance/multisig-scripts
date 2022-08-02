aa18f97bf3d4305c9b9fed007a51df7a7c5385ba
Michael Daly <dalymike102@gmail.com>
2022-08-02 18:14:27 -0400

    add script to update signers

---
+++ scripts/2022_08_02_update_multisig_signers.py
@@ -0,0 +1,39 @@
+from helpers import CHAIN_IDS, MULTISIG_ADDRESSES, GNOSIS_SAFE_ABI, OWNERS
+from ape_safe import ApeSafe
+from brownie import accounts, network, Contract
+
+from scripts.utils import confirm_posting_transaction
+
+
+def main():
+    """Update a vesting contract's ownership to company multisig"""
+
+    print(f"You are using the '{network.show_active()}' network")
+
+    deployer = accounts.load("deployer")  # prompts for password
+    multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
+
+    safe_contract = Contract.from_abi(
+        "Gnosis Safe",
+        MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]],
+        GNOSIS_SAFE_ABI,
+        multisig.account,
+    )
+    Kain_Warwick = "0x5b97680e165b4dbf5c45f4ff4241e85f418c66c2"
+
+    # prev owner(pointer), old owner, new owner
+    safe_contract.swapOwner(OWNERS[4], Kain_Warwick, OWNERS[5])
+
+    # new owner, threshold
+    safe_contract.addOwnerWithThreshold(OWNERS[0], 3)
+    assert safe_contract.getOwners() == OWNERS
+
+    # combine history into multisend txn
+    safe_tx = multisig.multisend_from_receipts()
+    safe_tx.safe_nonce = 54
+
+    # sign with private key
+    safe_tx.sign(deployer.private_key)
+    multisig.preview(safe_tx)
+
+    confirm_posting_transaction(multisig, safe_tx)
