91bcf8fd6ca4bbb0dc6d6ee133aacd363d1fbc6d
Michael Daly <dalymike102@gmail.com>
2022-08-08 12:57:24 -0400

    combined nonce 54 into mainnet signer update

--- scripts/2022_08_02_update_multisig_signers_mainnet.py
+++ scripts/2022_08_02_update_multisig_signers_mainnet.py
@@ -1,8 +1,16 @@
-from helpers import CHAIN_IDS, MULTISIG_ADDRESSES, OWNERS
+from helpers import (
+    CHAIN_IDS,
+    MULTISIG_ADDRESSES,
+    OWNERS,
+    SDL_ADDRESSES,
+    SDL_MINTER_ADDRESS,
+)
 from ape_safe import ApeSafe
 from brownie import accounts, network, Contract
 
 from scripts.utils import confirm_posting_transaction
+
+TARGET_NETWORK = "MAINNET"
 
 
 def main():
@@ -11,9 +19,9 @@ def main():
     print(f"You are using the '{network.show_active()}' network")
 
     deployer = accounts.load("deployer")  # prompts for password
-    multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
+    multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]])
 
-    safe_contract = multisig.contract(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
+    safe_contract = multisig.contract(MULTISIG_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]])
 
     Kain_Warwick = "0x5b97680e165b4dbf5c45f4ff4241e85f418c66c2"
 
@@ -23,6 +31,16 @@ def main():
     # new owner, threshold
     safe_contract.addOwnerWithThreshold(OWNERS[0], 3)
     assert safe_contract.getOwners() == OWNERS
+
+    # The amount to send to minter
+    amount_to_send = 5_000_000 * 1e18
+
+    # Target address
+    minter_address = SDL_MINTER_ADDRESS[CHAIN_IDS[TARGET_NETWORK]]
+
+    # Send sdl to minter
+    sdl = multisig.contract(SDL_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]])
+    sdl.transfer(minter_address, amount_to_send)
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
