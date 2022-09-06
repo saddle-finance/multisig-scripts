4f5af63fbdcb797a3dca7b93e44882e85bf08481
Hannes P <hannes.pfeiffer@gmx.at>
2022-09-06 03:14:21 +0200

    Fixed wrong nonce

--- scripts/2022_09_06_arb_update_minichef_for_week_9_8__9_15.py
+++ scripts/2022_09_06_arb_update_minichef_for_week_9_8__9_15.py
@@ -69,7 +69,7 @@ def main():
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
-    safe_tx.safe_nonce = 7
+    safe_tx.safe_nonce = 9
 
     # sign with private key
     safe_tx.sign(deployer.private_key)
