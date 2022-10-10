adb26caefb1e5e9980b358cbe7ee6d972117fedb
Michael Daly <dalymike102@gmail.com>
2022-10-10 12:52:31 -0400

    added october remunerations, added SDL sent to deployer for arb bridge

--- scripts/2022_10_10_68_set_votes_for_week_10_13__10_20_oct_remunerations.py
+++ scripts/2022_10_10_68_set_votes_for_week_10_13__10_20_oct_remunerations.py
@@ -4,10 +4,13 @@ from ape_safe import ApeSafe
 from brownie import Contract, accounts, network
 from helpers import (
     CHAIN_IDS,
+    DEPLOYER_ADDRESS,
     GAUGE_ABI,
     GAUGE_CONTROLLER_ADDRESS,
     MULTISIG_ADDRESSES,
+    SDL_ADDRESSES,
     SDL_DAO_COMMUNITY_VESTING_PROXY_ADDRESS,
+    SDL_MINTER_ADDRESS,
 )
 
 from scripts.utils import confirm_posting_transaction
@@ -16,10 +19,6 @@ TARGET_NETWORK = "MAINNET"
 
 
 def main():
-    """
-    Set Gauge weights for week 10_13_2022 -> 10_20_2022 from results of snapshot vote
-    Vote: https://snapshot.org/#/saddlefinance.eth/proposal/0xda1c8607e38db12bd5affe7891cb9d9e4eedb13fe7301afcb0e67d7abd10831a
-    """
 
     print(f"You are using the '{network.show_active()}' network")
     assert network.chain.id == CHAIN_IDS[TARGET_NETWORK], f"Not on {TARGET_NETWORK}"
@@ -27,10 +26,48 @@ def main():
     sdl_vesting_contract_proxy = multisig.contract(
         SDL_DAO_COMMUNITY_VESTING_PROXY_ADDRESS[CHAIN_IDS[TARGET_NETWORK]]
     )
-
-    # Release vested tokens to multisig account
-    sdl_vesting_contract_proxy.release()
+    sdl_contract = multisig.contract(SDL_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]])
     gauge_controller = multisig.contract(GAUGE_CONTROLLER_ADDRESS[CHAIN_IDS["MAINNET"]])
+
+    sdl_vesting_contract_proxy.release()
+
+    """Send the 6.7m SDL for October 2022 to affected LPs according to https://snapshot.org/#/saddlefinance.eth/proposal/0xf27b07ef7025aa23edb2a84d56825511ea7adff8d6164cd09a23c63c42cdc01a"""
+
+    refunds = {
+        "0xa0f75491720835b36edc92d06ddc468d201e9b73": 1552781.39214474,
+        "0x38ee5f5a39c01cb43473992c12936ba1219711ab": 1508913.00981469,
+        "0x38f9abd4bd8947da035abf51fc21244108a81ca6": 1132442.54883665,
+        "0x0ea4a285e1353f490ec7f473ab3174cac71cf79a": 1132219.93569692,
+        "0x6ef3f0ff397c3de367c6ca2cc3e6b6955e7993a5": 183256.672290299,
+        "0x6a73204db71f8e054bf9a0680b02ae96f700b595": 181265.62698562,
+        "0x5eea118e75f247014c6d0e990f02a0c254edc852": 151319.065792276,
+        "0x2b9d8f558e02753ed7d4d97dedfad662c02af3cb": 150998.563444325,
+        "0x0bb31c6278d58cff41b7e8ed3b20f76424fd69ad": 150936.201534769,
+        "0xc7d2a419f8f2eaa06b3811cf7faaa7440dcf96b7": 150888.579725597,
+        "0xae1e5c84b73136a49efb70a15149c350015391ed": 113255.253248794,
+        "0xe2d079278c0f4959199136f8db11c645d69a292b": 113234.843908429,
+        "0xffd4dae0d7d8ddb6f408dca0a47763ae3a57f4ce": 62280.638160798,
+        "0x9c7d894a29f97636b66dea646a1ae2f9c83a2830": 54450.629479509,
+        "0x77956a9d1b9901bbe4cfc2ed37db17500fbe7b0c": 37688.12936225,
+        "0x02ef8147e2d0997cca48d99f01bad846d16558fa": 30168.0509875,
+        "0xc7193c01f43257f3fe4c8f86784fcaa578a9e2b1": 24138.9591695,
+        "0xd716ad4f10eade5af4ed10006678f49bd2e6624f": 7545.4015315,
+        "0x99eb33756a2eaa32f5964a747722c4b59e6af351": 3214.8607335,
+        "0x2a1eaf114f2c23d30ef571d6e84fcbeb2f6f4e62": 1509.903056,
+        "0x46866d274e6d9015c5fdc098ce270803e11e3ef4": 829.988099,
+        "0x2d5d79753bb8e02ca1ce8ece0aa55288f8c8d840": 414.9940495,
+        "0xb5e46afa9596cb6f44d2fcb6dbd2096dbe35396c": 75.9582305,
+        "0x74f8744020f5900a2c3a3289c6c2182f56a6901f": 20.7935375,
+    }
+    total = sum(refunds.values())
+    print(f"Total amount to be refunded: {total} SDL")
+    for address, amount in refunds.items():
+        sdl_contract.transfer(address, amount * 1e18)
+
+    """
+    Set Gauge weights for week 10_13_2022 -> 10_20_2022 from results of snapshot vote
+    Vote: https://snapshot.org/#/saddlefinance.eth/proposal/0xda1c8607e38db12bd5affe7891cb9d9e4eedb13fe7301afcb0e67d7abd10831a
+    """
 
     gauge_to_relative_weight_dict = {
         "0xB2Ac3382dA625eb41Fc803b57743f941a484e2a6": 5765,  # FRAXBP Pool
@@ -75,6 +112,9 @@ def main():
     assert (
         9999 <= total_weight <= 10000
     ), f"Total weight must be 10000 but is {total_weight}"
+
+    # Send 500k SDL to deployer for manual bridging to Arbitrum Minichef to refill rewards
+    sdl_contract.transfer(DEPLOYER_ADDRESS, 500_000 * 1e18)
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
