3f9eda2d53e61a984617935e05a6e6c58cda4369
Hannes <hannes.pfeiffer@gmx.at>
2022-12-14 05:59:28 +0100

    Clear out Kava scritps

--- scripts/2022_xx_xx_4_0_kava_claim_admin_fees_kava.py
+++ scripts/2022_xx_xx_4_0_kava_claim_admin_fees_kava.py
@@ -16,28 +16,3 @@ def main():
 
     # skip until we've decided on a bridge
     return
-
-    print(f"You are using the '{network.show_active()}' network")
-    assert (network.chain.id == CHAIN_IDS[TARGET_NETWORK]), \
-        f"Not on {TARGET_NETWORK}"
-    multisig = ApeSafe(
-        MULTISIG_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]],
-    )
-    ops_multisig_address = OPS_MULTISIG_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]]
-
-    # Run any pending transactions before simulating any more transactions
-    # multisig.preview_pending()
-
-    claim_admin_fees(multisig, CHAIN_IDS[TARGET_NETWORK])
-
-    # combine history into multisend txn
-    # TODO: set 'safe_nonce'
-    safe_tx = multisig.multisend_from_receipts()
-    safe_nonce = 0
-
-    safe_tx.safe_nonce = safe_nonce
-
-    # sign with private key
-    safe_tx.sign(accounts.load("deployer").private_key)  # prompts for password
-    multisig.preview(safe_tx)
-    confirm_posting_transaction(multisig, safe_tx)
