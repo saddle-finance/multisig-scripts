72a76887d934d8008fff6e9df032aac11d070ef2
Michael Daly <dalymike102@gmail.com>
2022-12-23 15:28:59 -0500

    added claiming vested rewards

--- scripts/2022_12_24_79_send_SDL_to_minter.py
+++ scripts/2022_12_24_79_send_SDL_to_minter.py
@@ -4,7 +4,13 @@ from multiprocessing import pool
 from ape_safe import ApeSafe
 from brownie import accounts, network
 
-from helpers import CHAIN_IDS, MULTISIG_ADDRESSES, SDL_ADDRESSES, SDL_MINTER_ADDRESS
+from helpers import (
+    CHAIN_IDS,
+    MULTISIG_ADDRESSES,
+    SDL_ADDRESSES,
+    SDL_DAO_COMMUNITY_VESTING_PROXY_ADDRESS,
+    SDL_MINTER_ADDRESS,
+)
 from scripts.utils import confirm_posting_transaction
 
 
@@ -12,12 +18,19 @@ def main():
     """Add SimpleRewarder for T to minichef"""
 
     print(f"You are using the '{network.show_active()}' network")
+    TARGET_NETWORK = "MAINNET"
 
-    multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS["MAINNET"]])
-    sdl_contract = multisig.contract(SDL_ADDRESSES[CHAIN_IDS["MAINNET"]])
+    multisig = ApeSafe(MULTISIG_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]])
+    sdl_contract = multisig.contract(SDL_ADDRESSES[CHAIN_IDS[TARGET_NETWORK]])
+    sdl_vesting_contract_proxy = multisig.contract(
+        SDL_DAO_COMMUNITY_VESTING_PROXY_ADDRESS[CHAIN_IDS[TARGET_NETWORK]]
+    )
+
+    # Claim SDL from vesting contract
+    sdl_vesting_contract_proxy.release()
 
     # Send 3.5M SDL to SDL minter
-    minter = multisig.contract(SDL_MINTER_ADDRESS[CHAIN_IDS["MAINNET"]])
+    minter = multisig.contract(SDL_MINTER_ADDRESS[CHAIN_IDS[TARGET_NETWORK]])
     sdl_contract.transfer(minter.address, 3500000 * 1e18)
 
     # combine history into multisend txn
