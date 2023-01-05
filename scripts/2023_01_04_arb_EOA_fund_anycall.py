9ab1a63b17f3ea17cd193057d6bb447e5f205909
Jongseung Lim <penandlim@gmail.com>
2023-01-04 19:56:19 -0500

    Add EOA scripts for funding AnyCallv6

---
+++ scripts/2023_01_04_arb_EOA_fund_anycall.py
@@ -0,0 +1,35 @@
+from brownie import Contract, accounts, history, network
+
+from helpers import ANYCALL_V6_ADDRESS, CHAIN_IDS, get_contract_from_deployment
+
+TARGET_NETWORK = "ARBITRUM"
+
+
+def main():
+    """
+    Fund AnyCallV6 upto 0.1 ETH for AnyCallTranslator
+    """
+    print(f"You are using the '{network.show_active()}' network")
+    assert (
+        network.chain.id == CHAIN_IDS[TARGET_NETWORK]
+    ), f"Not on {TARGET_NETWORK} network"
+
+    anycall_v6 = Contract.from_explorer(ANYCALL_V6_ADDRESS)
+
+    any_call_translator = get_contract_from_deployment(
+        CHAIN_IDS[TARGET_NETWORK], "AnyCallTranslator")
+
+    current_budget = anycall_v6.executionBudget(any_call_translator.address)
+    print(
+        f"Current budget for AnyCallTranslator ({any_call_translator.address}) : {current_budget / 1e18} ETH")
+
+    # If the budget is less than 0.1e18, top it up
+    if current_budget < 0.09e18:
+        top_up_amount = 0.1e18 - current_budget
+        anycall_v6.deposit(
+            any_call_translator.address,
+            {"from": accounts.load("deployer"), "value": top_up_amount})
+
+    for tx in history:
+        tx.info()
+    return
