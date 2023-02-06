b8e10bdc06d4b56a88c91da31a6d3aa64f093e1e
Hannes <hannes.pfeiffer@gmx.at>
2023-02-06 18:54:00 +0100

    Fixed  field

--- scripts/2023_02_06_remunerations_January_plus_2m_minter_refill.py
+++ scripts/2023_02_06_remunerations_January_plus_2m_minter_refill.py
@@ -79,7 +79,8 @@ def main():
     sdl_vesting_contract_proxy.release()
 
     # Transfer 2m SDL to minter
-    sdl_contract.transfer(SDL_MINTER_ADDRESS[CHAIN_IDS["MAINNET"]], 2_000_000)
+    sdl_contract.transfer(SDL_MINTER_ADDRESS[CHAIN_IDS["MAINNET"]], 2_000_000, {
+                          "from": multisig.address})
 
     # combine history into multisend txn
     safe_tx = multisig.multisend_from_receipts()
