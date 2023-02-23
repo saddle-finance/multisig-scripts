a45962ec4f9562b5272fbdc394ea54102cb9568e
Hannes <hannes.pfeiffer@gmx.at>
2023-02-24 01:32:34 +0400

    Change name and nonce for mainnet claim script

--- scripts/2023_02_24_5_mainnet_claim_admin_fees.py
+++ scripts/2023_02_24_5_mainnet_claim_admin_fees.py
@@ -31,7 +31,7 @@ def main():
     # combine history into multisend txn
     # TODO: set 'safe_nonce'
     safe_tx = multisig.multisend_from_receipts()
-    safe_nonce = 0
+    safe_nonce = 86
 
     safe_tx.safe_nonce = safe_nonce
 
